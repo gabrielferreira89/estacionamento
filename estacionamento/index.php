2361b92aa801ada7c34a55d336f9e5f6028958b5
Gabriel Ferreira <gfinfo89@gmail.com>
2016-03-07 22:03:23 -0300

    Inclusão de estilo na página inicial do projeto.

--- estacionamento/index.php
+++ estacionamento/index.php
@@ -3,8 +3,10 @@
 <head>
 	<meta charset="UTF-8" />
 	<title>Página inicial do projeto</title>
+	<link rel="stylesheet" href="bootstrap/min.css" />
 </head>
 <body>
 	<h1>Página inicial do projeto</h1>
+	<?php print "Impresso em PHP";?>
 </body>
 </html>
