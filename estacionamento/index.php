5fec3d062f064644a741a1394fedba51b56268bb
Gabriel Ferreira <gfinfo89@gmail.com>
2016-03-07 21:54:41 -0300

    Estruturação da página inicial do projeto.

--- estacionamento/index.php
+++ estacionamento/index.php
@@ -1,2 +1,10 @@
-<?php
-print "Pagina Inicial do projeto!";
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Página inicial do projeto</title>
+</head>
+<body>
+	<h1>Página inicial do projeto</h1>
+</body>
+</html>
